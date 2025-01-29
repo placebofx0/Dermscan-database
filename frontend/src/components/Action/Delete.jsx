e2ce2e93459c58e258b88e5ab27522e3217d44b5
placebofx0 <patrizer3140@hotmail.com>
2025-01-30 00:46:11 +0700

    second commit

---
+++ frontend/src/components/Action/Delete.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import axios from "axios";
+
+const DeleteButton = ({ id, data, setData, filteredData, setFilteredData, API_URL }) => {
+    const handleDelete = async (id) => {
+        const confirmDelete = window.confirm("Are you sure you want to delete this record?");
+        if (confirmDelete) {
+            try {
+                await axios.delete(`http://localhost:8000/${API_URL}/${id}`);
+                setData(data.filter((item) => item._id !== id)); // ลบออกจาก state
+                setFilteredData(filteredData.filter((item) => item._id !== id)); // อัปเดต filteredData
+                alert("Record deleted successfully!");
+                } catch (error) {
+                console.error("Error deleting record:", error);
+                alert("Failed to delete record.");
+                }
+            }
+        };
+
+        return <button onClick={handleDelete}>Delete</button>;
+};
+
+export default DeleteButton;
+
+{/* <DeleteButton 
+    id={item._id} 
+    data={data} 
+    setData={setData} 
+    filteredData={filteredData} 
+    setFilteredData={setFilteredData} 
+    API_URL="your-api-endpoint"
+/> */}
